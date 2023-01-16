9e13bdffcdb55c820df8a7019b3563d2c73ff3c9
TheRand0mHer0 <amurdock@my365.bellevue.edu>
2023-01-15 20:22:38 -0700

    Format changes to previous commits

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -6,7 +6,7 @@ db = client.pytech
 
 def insert_one(student_obj):
     student_obj_id = db.students.insert_one(student_obj).inserted_id
-    print(f'Interested student {student_obj.first_name} {student_obj.last_name} into students collection with document id {student_obj_id}');
+    print(f'Inserted student {student_obj.first_name} {student_obj.last_name} into students collection with document_id {student_obj_id}');
     return 
 
 
@@ -14,7 +14,7 @@ student_one = { "student_id": 1008, "first_name": 'ant', "last_name": 'tony' }
 student_two = { "student_id": 1007, "first_name": 'yo', "last_name": 'lo' }
 student_three = { "student_id": 1009, "first_name": 'test', "last_name": 'test' }
 
-print('Inserting Students')
+print("-- INSERT STATEMENTS --")
 insert_one(student_one)
 insert_one(student_two)
 insert_one(student_three)
--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -6,8 +6,17 @@ db = client.pytech
 
 def find():
     all_students = db.students.find({})
+    for student in all_students:
+        print(f'Student Id: {student.student_id}\nFirst Name: {student.first_name}\nLast Name: {student.last_name}\n\n')
     return all_students
     
 def find_one(student_id):
     student = db.students.find_one({"student_id": student_id})
+    print(f'Student Id: {student.student_id}\nFirst Name: {student.first_name}\nLast Name: {student.last_name}\n\n')
     return student
+    
+
+print(" -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY -- ")
+find()
+print(" -- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY -- ")
+find_one(1008)
