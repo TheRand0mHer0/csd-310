16743b2e5b4cf4cccc700e2b1ada2ef061f2edc6
TheRand0mHer0 <amurdock@my365.bellevue.edu>
2023-02-11 01:07:14 -0700

    Update pysports_update_and_delete.py

--- module_9/pysports_update_and_delete.py
+++ module_9/pysports_update_and_delete.py
@@ -1,3 +1,6 @@
+# Anthony Murdock
+# Pysports Updates & Deletes
+
 import mysql.connector
 from mysql.connector import errorcode
 
@@ -11,6 +14,8 @@ config = {
 }
 
 get_player_records_command = "SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id"
+# a 2d nested array of commands needed for the assignment
+# index 0 is the cursor command and index 1 is the print statement
 cursor_commands = [
         [
             "INSERT INTO player (first_name, last_name, team_id) VALUES('Smeagol', 'Shire Folk', 1)",
@@ -41,7 +46,7 @@ try:
 
     input("\n\n Press any key to continue. . ." )
     cursor = db.cursor()
-  
+    # loop over cursor commands to execute
     for command in cursor_commands:
         cursor.execute(command[0])
         print(command[1])
